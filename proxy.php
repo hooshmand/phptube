0f924ea52a614b707056394a3ce169a7236a2013
M N Islam Shihan <mnishihan@gmail.com>
2012-11-14 18:42:47 +0600

    Fixed another issue in proxy.php

--- proxy.php
+++ proxy.php
@@ -44,7 +44,6 @@ if(!file_exists($file_path)){
 	$ch = curl_init($download_info['url']);
 	curl_setopt($ch, CURLOPT_FILE, $fp);
 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-	curl_setopt($ch, CURLOPT_RETURN_TRANSFER, false);
 	curl_exec($ch);
 	curl_close($ch);
 	fclose($fp);	
